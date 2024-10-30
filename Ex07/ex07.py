5f56d16ccdcd80737604959b8ea73d62e9dbcf9b
Edelson Santos <edelsonjr.4@hotmail.com>
2024-10-30 09:00:38 -0300

    Calcula Média

---
+++ Ex07/ex07.py
@@ -0,0 +1,8 @@
+#Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua média.
+
+num1 = int(input("Digite a nota do primeiro semestre: "))
+num2 = int(input("Digite a nota do segundo semestre: "))
+
+media = (num1 + num2) / 2
+
+print(f"A média entra as notas é {media}")
