5869379f39fb9d71667a459f60efbda5b7a73a50
Edelson Santos <edelsonjr.4@hotmail.com>
2024-10-30 21:51:15 -0300

    Create ex10.py

---
+++ Ex10/ex10.py
@@ -0,0 +1,10 @@
+#Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+
+
+dinheiro = float(input("Digite o quanto você tem na carteira? R$"))
+dolar = float(input("Digite o valor do dólar: "))
+
+
+dinheiro_em_dolar = dinheiro * dolar
+
+print(f"Se você trocar seu dinheiro para dólar, você terá ${dinheiro_em_dolar}")
