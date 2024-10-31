fb1be97b0f88131932f0f51d992d6ba804aafb41
Edelson Santos <edelsonjr.4@hotmail.com>
2024-10-30 21:53:46 -0300

    Update ex10.py

--- Ex10/ex10.py
+++ Ex10/ex10.py
@@ -5,6 +5,6 @@ dinheiro = float(input("Digite o quanto você tem na carteira? R$"))
 dolar = float(input("Digite o valor do dólar: "))
 
 
-dinheiro_em_dolar = dinheiro * dolar
+dinheiro_em_dolar = dinheiro / dolar
 
-print(f"Se você trocar seu dinheiro para dólar, você terá ${dinheiro_em_dolar}")
+print(f"Se você trocar seu dinheiro para dólar, você terá ${dinheiro_em_dolar:.2f}")
