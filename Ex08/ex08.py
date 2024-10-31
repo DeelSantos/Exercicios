7cddacc8a5d9befe7df9ed8ff31f5e7381d71212
Edelson Santos <edelsonjr.4@hotmail.com>
2024-10-30 21:27:57 -0300

    Create ex08.py

---
+++ Ex08/ex08.py
@@ -0,0 +1,12 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+metro = float(input("Digite o seu metro: "))
+
+
+km = metro / 1000
+
+
+
+
+
+print(f"Metro: {metro}m \n Kilometro: {km}km")
