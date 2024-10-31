5ef2f798eecd9a5cd81d60759b95815e230b290b
Edelson Santos <edelsonjr.4@hotmail.com>
2024-10-30 22:16:04 -0300

    Produto

---
+++ Ex12/ex12.py
@@ -0,0 +1,12 @@
+#Faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto.
+
+produto = float(input("Digite o valor do produto: R$"))
+
+desconto = (produto * 5) / 100
+
+novo_valor = produto - desconto
+
+print(f"O novo valor do seu desconto é {novo_valor:.2f}")
+
+
+
