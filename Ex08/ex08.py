1662ad73451a2ee0063793851c419a3cab9a2e74
Edelson Santos <edelsonjr.4@hotmail.com>
2024-10-30 21:32:33 -0300

    Calculando medidas

--- Ex08/ex08.py
+++ Ex08/ex08.py
@@ -4,9 +4,13 @@ metro = float(input("Digite o seu metro: "))
 
 
 km = metro / 1000
+hm = metro / 100
+dam = metro / 10
+dm = metro * 10
+cm = metro * 100
+mm = metro * 1000
 
 
 
 
-
-print(f"Metro: {metro}m \n Kilometro: {km}km")
+print(f"Metro: {metro}m \nKilometro:{km}km \nHectometro:{hm}hm \nDecametro:{dam}dam \nDecimetro:{dm}dm \nCentímetro:{cm}cm \nMilímetro:{mm}mm")
