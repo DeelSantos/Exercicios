001784e1a151aee7a03359ed627178822c12d874
Edelson Santos <edelsonjr.4@hotmail.com>
2024-10-30 21:40:06 -0300

    Tabuada em Python

---
+++ Ex09/ex09.py
@@ -0,0 +1,11 @@
+#Faça um programa que leia um número Inteiro qualquer e mostre na tela a sua tabuada.
+
+numero = int(input("Digite a sua tabuada: "))
+
+print(f"Tabuada do {numero}")
+
+for i in range(0, 11):
+    print(f"{numero} x {i}:", numero * i)
+    
+    
+    
